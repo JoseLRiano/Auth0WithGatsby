02435bd63cd08232ac1fb8809c120f18c9004967
JoseLRiano <joselriano@yahoo.com>
2020-12-20 23:42:28 -0500

    Profile page and gatsby node file created. client only route and redirection implemented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import {Link} from "gatsby";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <Link to="/profile">Go to your profile</Link>
+    </div>)
 }
---
+++ src/pages/profile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const profile = () => {
+    return (
+        <div>
+            <p>This is your profile dashboard</p>
+        </div>
+    )
+}
+export default profile;
