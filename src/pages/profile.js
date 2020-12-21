fcc7c447467f9b2198ad2bdc49c7606eb181aa6d
JoseLRiano <joselriano@yahoo.com>
2020-12-20 23:57:46 -0500

    Router reach added, and some simple example components were added to the profile route

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Router } from '@reach/router';
+import { Link } from 'gatsby';
+
+const Home = () => <p>Home</p>;
+const Settings = () => <p>Settings</p>;
+const Billing = () => <p>Billing</p>;
 
 const profile = () => {
     return (
         <div>
+            <nav>
+                <Link to="/profile">Home</Link>
+                <Link to="/profile/settings">Settings</Link>
+                <Link to="/profile/billing">Billing</Link>
+            </nav>
             <p>This is your profile dashboard</p>
+            <Router>
+                <Home path="/profile"/>
+                <Settings path="/profile/settings"/>
+                <Billing path="/profile/billing"/>
+            </Router>
         </div>
     )
 }
