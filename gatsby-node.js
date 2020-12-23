34b09d746c791a720b874ef3e3791945c1744ceb
JoseLRiano <joselriano@yahoo.com>
2020-12-23 01:44:13 -0500

    A Function to ignore auth0 during server side build and a new folder with fucntion to check if there is a window on the brosers so auth0 can run were added

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,3 +13,27 @@ exports.onCreatePage = async ({ page, actions }) => {
         createPage(Page)
     }
 }
+
+// This code ignores auth0-js during the server-side build since it relies on browser APIs like `window`
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if(stage === "build-html"){
+        /*
+        * During the build step, `auth0-js` will break because it relies on 
+        * Browser-specific APIs. Fortunately , we don't need it during the build.
+        * Using Webpack's null loades, we're able to effectivelu ignore `auth0-js`
+        * during the build. (See `src/utils/auth.js` to see how we prevent this 
+        * from breaking the app.)
+        */
+       actions.setWebpackConfig({
+           module: {
+               rules: [
+                   {
+                       test: /auth0-js/,
+                       use: loaders.null(),
+                   },
+               ],
+           },
+       })
+    }
+}
