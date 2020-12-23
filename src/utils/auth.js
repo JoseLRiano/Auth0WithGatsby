3691034dcb720033e00723b186e4ec423d993845
JoseLRiano <joselriano@yahoo.com>
2020-12-23 17:47:03 -0500

    set the session for auth0

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import { navigate } from 'gatsby';
 
 const isBrowser = typeof window !== "undefined";
 
@@ -11,3 +12,59 @@ const auth = isBrowser
         scope: "openid profile email",
         })
     : {};
+
+const tokens = {
+    accessToken: false,
+    idToken: false,
+    expiresAt: false,
+};
+
+let user = {};
+
+export const isAuthenticated = () => {
+    if(!isBrowser) {
+        return;
+    }
+
+    return localStorage.getItem("isLoggedIn") === "true";
+}
+
+export const login = () => {
+    if(!isBrowser){
+        return;
+    }
+
+    auth.authorize();
+}
+
+const setSession = ( cb = () => {}) => (err, authResult) => {
+    if(err){
+        navigate('/');
+        cd();
+        return;    
+    }
+
+    if(authResult && authResult.accessToken && authResult.idToken){
+        let expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
+        tokens.accessToken = authResult.accessToken;
+        tokens.idToken = authResult.idToken;
+        tokens.expiresAt = expiresAt;
+        user = authResult.idTokenPayLoad;
+        localStorage.setItem("isLoggedIn",  true);
+        navigate('/profile');
+        cd();
+    }
+}
+
+export const hanldeAuthentication = () => {
+    if(!isBroser){
+        return;
+    }
+
+    auth.parseHash(setSession());
+
+}
+
+export const getProfile = () => {
+    return user;
+}
